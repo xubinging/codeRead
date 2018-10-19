ca7b292b9b6d8bc79b59ea2224eed57c64d55aeb
xubing <1379196929@qq.com>
2018-10-19 09:37:47 +0800

    初建

---
+++ Podfile
@@ -0,0 +1,27 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'codeRead' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+
+  # Pods for codeRead
+  pod 'AFNetworking', '3.1.0'
+  pod 'SDWebImage', '3.8.2'
+  pod 'Masonry', '1.1.0'
+  pod 'MBProgressHUD', '1.0.0'
+  pod 'MJRefresh', '3.1.14.1'
+  pod 'YYKit', '1.0.9'
+
+
+  target 'codeReadTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'codeReadUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ codeReadTests/codeReadTests.m
@@ -0,0 +1,39 @@
+//
+//  codeReadTests.m
+//  codeReadTests
+//
+//  Created by migu on 2018/10/19.
+//  Copyright © 2018年 smile. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface codeReadTests : XCTestCase
+
+@end
+
+@implementation codeReadTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
